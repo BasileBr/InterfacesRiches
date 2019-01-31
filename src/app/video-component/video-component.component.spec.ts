7b759e2652ee28a3b20b3aec084132b7d7c64683
Yvan Ulas <yulas@enssat.fr>
2019-01-31 11:58:22 +0100

    add chapters

--- src/app/video-component/video-component.component.spec.ts
+++ src/app/video-component/video-component.component.spec.ts
@@ -28,4 +28,14 @@ describe('VideoComponentComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector("video")).toBeTruthy();
   });
+
+
+  it('video should have all sources', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    const sources = compiled.querySelector("video").querySelectorAll("source");
+    sources.forEach(function (currentValue, currentIndex, listObj) {
+    expect(currentValue.type).toMatch(/video\/mp4|video\/ogg/);
+  });
+});
+
 });
